d7c43cc7d55cc39f044c74853a8050989e1c08db
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-25 20:03:39 +0200

    Finished the MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
-const express = require ('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const app = express()
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(8800, () => {
-    console.log("Backend server is running!");
-})
+  console.log("Backend server is running!");
+});
