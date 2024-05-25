dcc5a08a759b734bfd9bc10d8e01d4831689ad37
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-05-25 21:13:54 +0200

    Finished user register

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const pinRoute = require("./routes/pins")
+const userRoute = require("./routes/users")
 
 const app = express();
 
@@ -16,7 +17,8 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-  app.use("/api/pins", pinRoute)
+app.use("/api/pins", pinRoute)
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
